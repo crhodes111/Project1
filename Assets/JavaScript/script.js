ca0bc4cfb6d1a9f643f2dacb8269de92c10de831
chriscoding1 <cltmedicman@gmail.com>
2021-09-13 15:30:24 -0400

    Changed banner width, Fixed javascript, edited select box
    Changed banner width, Fixed javascript, edited select box

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,7 +1,6 @@
-var country = "Japan";
-var countryId = 'JP';
-var amadeusApi = 'v2VHGvDzvKkUDZ4dYmrvGtgXbIryxVoC';
-var amadeusSecret = 'BXi2TubLGNGhXqEC';
+var countryArr = ['New Zealand', 'France', 'Mexico', 'USA', 'French Polynesia', 'England', 'Italy', 'Thailand', 'Japan', 'Spain', 'UAE', 'Canada', 'USA', 'Indonesia', 'Australia']
+var countryIdarr = ['NZ', 'FR', 'MX', 'US', 'PF', 'GB', 'IT', 'TH', 'JP', 'ES', 'AE', 'CA', 'US', 'ID', 'AU'];
+var cityArr = ['Christchurch,NZ', 'Paris,FR', 'Cancun,MX', 'San Francisco,US', 'Papeete,PF', 'London,GB', 'Rome,IT', 'Phuket,TH', 'Tokyo,JP', 'Barcelona,ES', 'Dubai,AE', 'Banff,CA', 'New York City,US', 'Bali,ID', 'Sydney,AU'];
 
 var adviseEl = document.querySelector('#advise');
 var waterEl = document.querySelector('#water');
@@ -9,68 +8,75 @@ var currencyEl = document.querySelector('#currency');
 var languageEl = document.querySelector('#language');
 var vaccineEl = document.querySelector('#vaccine');
 var webcamEL = document.querySelector('#webcam');
-
-travelBrief(country);
+var listEl = document.querySelector('#destination');
 
 function travelBrief (country) {
     fetch('https://travelbriefing.org/' + country + '?format=json')
         .then(function(response) {return response.json()})
         .then(function(data) {
-            //console.log(data);
-            //console.log("More travel info: " + data.advise.CA.url);
             adviseEl.innerHTML = "<p>More travel info: <a href=\"" + data.advise.CA.url + "\">" + data.advise.CA.url + "</p>";
-            //console.log("Drinking water safety: " + data.water.short);
+            
             waterEl.innerHTML = "<p>Water Quality: " + data.water.short + "</p>";
-            //console.log("Currency code: " + data.currency.code);
+            
             currencyEl.innerHTML = "<p>Currency Code: " + data.currency.code + "</p><p>Currency: " +
             data.currency.name + "</p>";
-            //console.log("Currency: " + data.currency.name);
+            
             languageEl.innerHTML = "";
             if (data.language.length > 1) {
                 for (i = 0; i < data.language.length; i++) {
-                    //console.log("Language " + (i+1) + ": " + data.language[i].language);
+                    
                     var p = document.createElement("p");
                     p.innerHTML = "Language " + (i+1) + ": " + data.language[i].language;
                     languageEl.appendChild(p);
                 }
             } else {
-                //console.log(data.language[0].language);
+                
                 languageEl.innerHTML = "Language: " + data.language[0].language;
             }
             vaccineEl.innerHTML = "";
             if (data.vaccinations.length > 0) {
                 for (i = 0; i < data.vaccinations.length; i++) {
-                    //console.log(data.vaccinations[i].name + ": " + data.vaccinations[i].message);
                     var pV = document.createElement("p");
                     pV.innerHTML = data.vaccinations[i].name + ": " + data.vaccinations[i].message;
                     vaccineEl.appendChild(pV);
                 }
             } else {
-                //console.log("No vaccinations recommended");
                 vaccineEl.innerHTML = "No vaccinations recommended";
             }
             
     })
 }
 
-webCam();
-
-function webCam () {
+function webCam (countryId) {
     fetch('https://api.windy.com/api/webcams/v2/list/country=' + countryId + '?show=webcams:location,image&key=DucimssCbEzOJVh8c8yJe1rLBHnDXfBz')
     .then(function(response) {return response.json()})
     .then(function(data) {
-        //console.log(data.result.webcams[Math.trunc(Math.random()*data.result.webcams.length)].image.daylight.preview);
-        var pW = document.createElement('img');
-        pW.src = data.result.webcams[Math.trunc(Math.random()*data.result.webcams.length)].image.daylight.preview;
-        webcamEL.appendChild(pW);
+        
+        webcamEL.innerHTML = "";
+        if (data.result.webcams.length > 0) {
+            var pW = document.createElement('img');
+            pW.src = data.result.webcams[Math.trunc(Math.random()*data.result.webcams.length)].image.daylight.preview;
+            webcamEL.appendChild(pW);
+        } else {
+            var pW = document.createElement('p');
+            pW.innerText = "No Webcams to Display";
+            webcamEL.appendChild(pW);
+        }
+        
     })
 }
 
 $('.destination').on('click', function (event) {
     event.preventDefault()
+    
+    var value = listEl.value;
+    var cityName = listEl.options[listEl.selectedIndex].text;
+    let destinationTarget = cityArr[value];
 
-    //let destinationTarget = event.target.innerHTML
-    let destinationTarget = 'Tokyo,Japan';
+    webCam(countryIdarr[value]);
+    travelBrief(countryArr[value]);
+    
+
     const apiKey = '668cdb30df14e3d9284e2e3a36347615'
 
     fetch(
@@ -92,7 +98,7 @@ $('.destination').on('click', function (event) {
 
             console.log(destinationIconData);
 
-            $('#weatherLocation').text(destinationTarget)
+            $('#weatherLocation').text(cityName)
             $('#weatherIcon').attr('src', destinationIconURL)
             $('#temp').text(`Temp: ${destinationTemp} ℉`)
             $('#wind').text(`Wind: ${destinationWind} MPH`)
